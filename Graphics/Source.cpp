1aa02f91f3bc724802586c302062259e0c958465
bluebean067 <70041025+bluebean067@users.noreply.github.com>
2025-09-29 18:58:16 -0500

    Added time variable, and time float uniform in fragment shader to change transparency of cat.

--- Graphics/Source.cpp
+++ Graphics/Source.cpp
@@ -94,11 +94,11 @@ int main() {
         out vec4 outColor;
         uniform sampler2D texKitten;
         uniform sampler2D texPuppy;
-        
+        uniform float time;
         void main() {
             vec4 colKitten = texture(texKitten, Texcoord);
             vec4 colPuppy = texture(texPuppy, Texcoord);
-            outColor = mix(colKitten, colPuppy, 0.5);
+            outColor = mix(colKitten, colPuppy, time);
         }
     )glsl";
     //Create Id to store the shader
@@ -240,8 +240,11 @@ int main() {
     auto t_start = std::chrono::high_resolution_clock::now();
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+        auto t_now = std::chrono::high_resolution_clock::now();
+        float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
 
-        
+		GLint timeUnform = glGetUniformLocation(shaderProgram, "time");
+		glUniform1f(timeUnform, (sin(time) + 1.0f) / 2.0f);
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
